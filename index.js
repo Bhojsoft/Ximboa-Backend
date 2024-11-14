bf05f6e88487df97ca39b19f727f965aeaabb03b
Mahesh <maheshzalte2000@gmail.com>
2024-11-14 13:05:55 +0530

    settingup cors policy https to http

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(
   cors({
-    origin: "https://ximboa.com",
+    origin: "http://ximboa.com",
     methods: "GET, POST, PUT, DELETE",
     credentials: true,
   })
